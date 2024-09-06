c19cb3eeb2a98436893e186868bf552915c030ff
FatimaBartels <bartels.fatima@gmail.com>
2024-09-06 10:10:34 +0200

    added the basic structure

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
+import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather APP</h1>
+      <div className="container">
+        <Weather />
 
-      <footer>
-        <p>
-          This app was coded by{" "}
-          <a
-            href="https://www.fatimabartels.com/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Fatima Bartels
-          </a>{" "}
-          and is open-sourced on{" "}
-          <a
-            href="https://github.com/FatimaBartels/weather-app-react"
-            target="_blank"
-            rel="noreferrer"
-          >
-            GitHub
-          </a>
-        </p>
-      </footer>
+        <footer>
+          <p>
+            This app was coded by{" "}
+            <a
+              href="https://www.fatimabartels.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Fatima Bartels
+            </a>{" "}
+            and is open-sourced on{" "}
+            <a
+              href="https://github.com/FatimaBartels/weather-app-react"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub
+            </a>
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
